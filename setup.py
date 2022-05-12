837fecf5b362946778b0b71da8a6be049de3baec
Deepak Mishra <deepak.mishra2@sanofi.com>
2022-05-12 06:56:13 +0000

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="pip-pkg-demo1",
+    version="0.0.1",
+    author="Deepak Mishra",
+    author_email="deepak.mishra2@gmail.com",
+    description="A demo pip package for Gitlab pacakge resistry",
+    package=find_packages(),
+    classifiers=["Python 3.9", "Gitlab Demo", "Cross Platfomr"],
+    python_requires='>=3.5'
+)
